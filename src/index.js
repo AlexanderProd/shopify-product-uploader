8fa3e949d4f772ff0ccf71a7fa4baf70accb2089
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-07 23:33:32 +0100

    added progress bar

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ const Shopify = require('shopify-api-node')
 const image2base64 = require('image-to-base64')
 const rtfToHTML = require('@iarna/rtf-to-html')
 const { createReadStream } = require('fs')
+const _cliProgress = require('cli-progress')
  
 const DIR = '/Users/alexanderhoerl/Dropbox/H2/Bio Balance/Inhalte_Web/Produkte/Tier/Good\ BEE\ Probiotic'
 const shopify = new Shopify({
   shopName: process.env.SHOP_NAME,
   apiKey: process.env.APIKEY,
   password: process.env.PASSWORD,
-});
+})
 
 const createHtmlDescription = (rtfFilePath) => {
   const outputTemplate = (doc, defaults, content) => (
@@ -27,53 +28,52 @@ const createHtmlDescription = (rtfFilePath) => {
   })
 }
 
-const createProduct = async (product) => {
-  try {
-    await shopify.product.create(product)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 const main = async () => {
+  const bar = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic)
   const tree = dirTree(DIR)
-  tree.children.map(async (child) => {
-    if(child.type === 'file'){
-      return
-    }
 
-    const name = child.name
-    const productImagesPaths = []
-    const images = []
-    const rtfFile = []
+  await bar.start(2, 0)
 
-    if(child.children){
-      child.children.map(x => {
-        if (['.jpeg', '.png', '.jpg'].includes(x.extension)){
-          productImagesPaths.push(x.path)
-        }
-        if(['.rtf'].includes(x.extension)){
-          rtfFile.push(x.path)
-        }
+  const mapChildren = Promise.all(
+    tree.children.map(async (child, index) => {
+      if(child.type === 'file'){ return }
+  
+      const name = child.name
+      const productImagesPaths = []
+      const images = []
+      const rtfFile = []
+      const htmlDescription = await createHtmlDescription(rtfFile[0])
+  
+      if(child.children){
+        child.children.map(x => {
+          if (['.jpeg', '.png', '.jpg'].includes(x.extension)){
+            productImagesPaths.push(x.path)
+          }
+          if(['.rtf'].includes(x.extension)){
+            rtfFile.push(x.path)
+          }
+        })
+      }
+  
+      const createBase64 = Promise.all(productImagesPaths.map(async (path) => {
+        const base64Code = await image2base64(path)
+        images.push({
+          "attachment": base64Code,
+        },)
+      }))
+      await createBase64
+
+      await shopify.product.create({
+          "title": name,
+          "body_html": htmlDescription,
+          "images": images,
       })
-    }
-
-    const createBase64 = Promise.all(productImagesPaths.map(async (path) => {
-      const base64Code = await image2base64(path)
-      images.push({
-        "attachment": base64Code,
-      },)
-    }))
-    await createBase64
-
-    const htmlDescription = await createHtmlDescription(rtfFile[0])
-
-    createProduct({
-        "title": name,
-        "body_html": htmlDescription,
-        "images": images,
+      bar.update(index)
     })
-  })
+  )
+  await mapChildren
+
+  bar.stop()
 }
 
 main()
