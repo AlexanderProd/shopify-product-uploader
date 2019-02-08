3e3a52aa0b9061c58b60d9e92776b831d1bb2729
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-08 17:51:03 +0100

    added productDetail from JSON parsing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const dirTree = require("directory-tree")
 const Shopify = require('shopify-api-node')
 const image2base64 = require('image-to-base64')
 const rtfToHTML = require('@iarna/rtf-to-html')
-const { createReadStream } = require('fs')
+const { createReadStream, readFile } = require('fs')
 const _cliProgress = require('cli-progress')
 
 const DIR = process.argv[2] || '/Users/alexanderhoerl/Dropbox/H2/Bio Balance/Inhalte_Web/Produkte/Tier/Good\ BEE\ Probiotic'
@@ -21,8 +21,18 @@ const createHtmlDescription = async rtfFilePath => {
   const options = { template: outputTemplate }
   return new Promise((resolve, reject) => {
     rtfToHTML.fromStream(createReadStream(rtfFilePath), options, (err, html) => {
-      if(err) reject(err)
+      if (err) reject(err)
       resolve(html)
+    })
+  }).catch(err => console.error(err))
+}
+
+const createProductDetails = async jsonFilePath => {
+  if (!jsonFilePath) return
+  return new Promise((resolve, reject) => {
+    readFile(jsonFilePath, 'utf-8', (err, data) => {
+      if (err) reject(err)
+      resolve(JSON.parse(data))
     })
   }).catch(err => console.error(err))
 }
@@ -43,6 +53,7 @@ const main = async () => {
   const foldersToDo = await countFolders(tree)
   let progress = 0
 
+  console.log(`Uploading ${foldersToDo} products!`)
   await bar.start(foldersToDo, progress)
 
   const mapChildren = Promise.all(
@@ -53,7 +64,8 @@ const main = async () => {
       const productImagesPaths = []
       const images = []
       const rtfFile = []
-      
+      const jsonFile = []
+
       if (child.children){
         child.children.map(x => {
           if (['.jpeg', '.png', '.jpg'].includes(x.extension)){
@@ -61,6 +73,9 @@ const main = async () => {
           }
           if (['.rtf'].includes(x.extension)){
             rtfFile.push(x.path)
+          }
+          if (['.json'].includes(x.extension)){
+            jsonFile.push(x.path)
           }
         })
       }
@@ -74,8 +89,10 @@ const main = async () => {
       await createBase64
 
       const htmlDescription = await createHtmlDescription(rtfFile[0])
+      const productDetails = await createProductDetails(jsonFile[0])
 
       await shopify.product.create({
+          ...productDetails,
           "title": name,
           "body_html": htmlDescription,
           "images": images,
